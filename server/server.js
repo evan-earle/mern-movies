b8c0ece5d9298943f57746c8dbe25afd38220376
Evan Earle <evan.earle@gmail.com>
2024-02-14 11:06:00 -0400

    formatting

--- server/server.js
+++ server/server.js
@@ -38,7 +38,6 @@ app.use((err, req, res, next) => {
 
 //Deployment
 app.use(express.static(path.join(__dirname, "../client/dist")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
