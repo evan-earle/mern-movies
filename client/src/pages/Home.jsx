bb610b3df6e6c1503002593cdb3b03aa242d9186
Evan Earle <evan.earle@gmail.com>
2024-03-05 15:34:00 -0400

    changed css classes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ export const Home = () => {
   }, [auth, navigate]);
 
   return (
-    <div className="min-h-full h-screen flex items-center flex-col py-6 px-4 sm:px-6 lg:px-8 bg-no-repeat bg-cover bg-center  ">
+    <div className="min-h-full h-screen flex items-center flex-col py-6 px-4">
       <h1 className="w-1/3 flex justify-center mb-3 p-4 rounded bg-slate-50 text-3xl max-lg:w-2/3 max-sm:w-full">
         FilmFindr
       </h1>
